695eb69964aa65199aa2ff99ca34de865082d431
ahmadfma <ahmadfathanah05@gmail.com>
2023-08-16 08:35:12 +0800

    login page

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:legalkan/common/dimensions.dart';
 import 'package:legalkan/common/styles.dart';
 import 'package:legalkan/locator.dart';
@@ -15,7 +16,6 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -25,69 +25,105 @@ class LoginPage extends StatelessWidget {
                 padding: const EdgeInsets.only(left: defaultMarginSize, right: defaultMarginSize, bottom: extraBixMarginSize),
                 child: Column(
                   children: [
-                    SizedBox(height: size.height * 0.1,),
-                    Wrap(
-                      direction: Axis.horizontal,
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Image.asset("assets/images/logo_unhas.png", height: 60,),
-                        const SizedBox(width: 30,),
-                        const Icon(Icons.close, color: lightRed,),
-                        const SizedBox(width: 30,),
-                        Image.asset('assets/images/logo_kampus_merdeka.png',height: 60,)
-                      ],
-                    ),
-                    const SizedBox(height: 30,),
-                    Text("Universitas Hasanuddin", style: myTextTheme.titleLarge?.copyWith(fontSize: 18, fontWeight: FontWeight.w700),),
-                    Text.rich(
-                        TextSpan(
-                            children: [
-                              TextSpan(
-                                  text: "Kampus ",
-                                  style: myTextTheme.titleLarge?.copyWith(fontSize: 14, fontWeight: FontWeight.w700, color: primaryColor)
-                              ),
-                              TextSpan(
-                                  text: "Merdeka",
-                                  style: myTextTheme.titleLarge?.copyWith(fontSize: 14, fontWeight: FontWeight.w700, color: blue)
-                              )
-                            ]
-                        )
-                    ),
+                    const SizedBox(height: 50,),
+                    Image.asset('assets/images/logo.png', height: 100,),
                     const SizedBox(height: 50,),
                     CustomTextField(
                       hint: 'Username',
-                      label: 'Masukkan NIM anda',
                       boxDecoration: BoxDecoration(
-                        border: Border.all(color: lightBlue2, width: 1),
+                        border: Border.all(color: greyBackground, width: 1),
                         borderRadius: const BorderRadius.all(Radius.circular(8)),
-                        color:  lightBlue4
+                        color:  backgroundColor
                       ),
-                      onChanged: (value) {
-
-                      },
+                      onChanged: (value) {},
                     ),
-                    const SizedBox(height: bigMarginSize,),
+                    const SizedBox(height: defaultMarginSize,),
                     CustomTextField(
                       hint: 'Password',
                       isObscureText: true,
-                      label: 'Masukkan password anda',
                       boxDecoration: BoxDecoration(
-                          border: Border.all(color: lightBlue2, width: 1),
+                          border: Border.all(color: greyBackground, width: 1),
                           borderRadius: const BorderRadius.all(Radius.circular(8)),
-                          color:  lightBlue4
+                          color:  backgroundColor
                       ),
-                      onChanged: (value) {
-
-                      },
+                      onChanged: (value) {},
                     ),
-                    const SizedBox(height: superBigMarginSize
-                      ,),
+                    const SizedBox(height: bigMarginSize,),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Text("Lupa Sandi ?", style: myTextTheme.bodyMedium?.copyWith(fontSize: 14, fontWeight: FontWeight.w500, color: blue), textAlign: TextAlign.end),
+                    ),
+                    const SizedBox(height: bigMarginSize,),
                     CustomButton(
+                      textStyle: myTextTheme.labelLarge?.copyWith(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w600),
                       onPressed: () {
                         locator<NavigationCustom>().navigateReplace(UserMainPage.routeName);
-                        //Navigator.of(context).pushNamedAndRemoveUntil(MainPage.routeName, ModalRoute.withName('/'));
                       },
                       text: 'Masuk',
+                    ),
+                    const SizedBox(height: superBigMarginSize,),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            color: greyBackground,
+                          ),
+                        ),
+                        const SizedBox(width: smallMarginSize,),
+                        Text("Atau masuk dengan", style: myTextTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500, color: Colors.grey[600]),),
+                        const SizedBox(width: smallMarginSize,),
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            color: greyBackground,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: bigMarginSize,),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: smallMarginSize),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: greyBackground, width: 1),
+                              borderRadius: BorderRadius.all(Radius.circular(4))
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset('assets/icons/facebook_ic.svg', height: 25),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: defaultMarginSize,),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: smallMarginSize),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: greyBackground, width: 1),
+                                borderRadius: BorderRadius.all(Radius.circular(4))
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset('assets/icons/google_ic.svg', height: 25),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: defaultMarginSize,),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: smallMarginSize),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: greyBackground, width: 1),
+                                borderRadius: BorderRadius.all(Radius.circular(4))
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset('assets/icons/cib_apple.svg', height: 25),
+                            ),
+                          ),
+                        ),
+
+                      ],
                     )
                   ],
                 ),
@@ -105,7 +141,14 @@ class LoginPage extends StatelessWidget {
                   onTap: () {
                     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Belum tersedia saat ini")));
                   },
-                  child: Text("Butuh Bantuan ? ", style: myTextTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w700, color: blue),),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Belum memiliki akun ? "),
+                      const SizedBox(width: smallMarginSize,),
+                      Text("Daftar Sekarang", style: myTextTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w700, color: blue),),
+                    ],
+                  ),
                 ),
                 const SizedBox(height: defaultMarginSize,),
               ],
